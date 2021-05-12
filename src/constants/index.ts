dad0e57034068e1545efdb5f45918b2fb633261a
silvercondor <biocrypto@protonmail.com>
2021-05-13 01:15:14 +0800

    Change router address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap-libs/sdk'
 
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+export const ROUTER_ADDRESS = '0xD99D1c33F9fC3444f8101754aBC46c52416550D1'
 
 // a list of tokens by chain
 type ChainTokenList = {
